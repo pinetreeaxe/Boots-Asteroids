32b5e05126147d1aa25894acfbddbfea40feaed1
Pinetreeaxe <jmachado771@gmail.com>
2025-11-02 17:50:44 +0000

    shooting asteroids

--- main.py
+++ main.py
@@ -36,15 +36,14 @@ def main():
         updatable.update(dt)
 
         for asteroid in asteroids:
-            for shot in shots:
-                if asteroid.collides_with(shot):
-                    asteroid.kill()
-                    shot.kill()
-                    break
-
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.kill()
 
         screen.fill("black")
 
