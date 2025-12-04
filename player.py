7c3718cfa15ea34f0b89d7eba90915b66b2f8179
Pinetreeaxe <jmachado771@gmail.com>
2025-12-04 21:04:02 +0000

    color changes

--- player.py
+++ player.py
@@ -18,7 +18,7 @@ class Player(CircleShape):
         self.shoot_timer = 0
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+        pygame.draw.polygon(screen, "yellow1", self.triangle(), LINE_WIDTH)
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
